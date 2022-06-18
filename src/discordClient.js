a7cb576b6f0e9c9568e353e43ac89248cd579173
Joel DeSante <joeldesante@gmail.com>
2022-06-18 19:29:22 -0400

    Add reaction listeners

--- src/discordClient.js
+++ src/discordClient.js
@@ -1,11 +1,14 @@
 import { Client, Intents } from "discord.js";
 import { Logger } from "./logger.js";
 
-const client = new Client({ intents: [ 
-    Intents.FLAGS.GUILDS, 
-    Intents.FLAGS.GUILD_MESSAGES,
-    Intents.FLAGS.GUILD_MESSAGE_REACTIONS 
-]});
+const client = new Client({ 
+    intents: [ 
+        Intents.FLAGS.GUILDS, 
+        Intents.FLAGS.GUILD_MESSAGES,
+        Intents.FLAGS.GUILD_MESSAGE_REACTIONS
+    ],
+    partials: ['MESSAGE', 'CHANNEL', 'REACTION'],
+});
 
 client.once('ready', () =>  {
     Logger.info("Harvey has logged in and is ready.");
--- src/index.js
+++ src/index.js
@@ -9,4 +9,30 @@ if(TOKEN.toString().trim() === "") {
     throw new Error("Invalid Discord access token configured.");
 }
 
+client.on('messageReactionAdd', async (reaction, user) => {
+	if (reaction.partial) {
+		try {
+			await reaction.fetch();
+		} catch (error) {
+			Logger.error('Something went wrong when fetching the message:', error);
+			return;
+		}
+	}
+
+	Logger.info(`${reaction.message.author}'s message "${reaction.message.content}" lost a reaction!`);
+});
+
+client.on('messageReactionRemove', async (reaction, user) => {
+	if (reaction.partial) {
+		try {
+			await reaction.fetch();
+		} catch (error) {
+			Logger.error('Something went wrong when fetching the message:', error);
+			return;
+		}
+	}
+
+	Logger.info(`${reaction.message.author}'s message "${reaction.message.content}" lost a reaction!`);
+});
+
 client.login(TOKEN);
