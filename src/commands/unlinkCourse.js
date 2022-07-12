0e53468dcf049b4922408af69e72cfa5a6f0e514
Joel DeSante <joeldesante@gmail.com>
2022-07-12 14:59:40 -0400

    Add unlink command

---
+++ src/commands/unlinkCourse.js
@@ -0,0 +1,22 @@
+import { SlashCommandBuilder, SlashCommandChannelOption, SlashCommandRoleOption, SlashCommandStringOption } from '@discordjs/builders';
+import { linkExistingCourseChannel } from '../lib/courseChannels.js';
+
+export default {
+    body: new SlashCommandBuilder()
+        .setName('unlink-course')
+        .setDescription('Migrates the old channels to the new system.')
+        .addRoleOption(
+            new SlashCommandRoleOption()
+                .setName("course-role")
+                .setDescription("The role which is associated with the chat channel.")
+                .setRequired(true)
+        )
+        .setDefaultMemberPermissions(0)
+        .setDMPermission(false),
+    onTriggered: async function(interaction) {
+        await unlinkExistingCourseChannel(
+            interaction.options.getRole("course-role").id
+        );
+        interaction.reply("Unlinked role successfully");
+    }   
+};
