fd70aef933ed4d2768ee4c000d5fad7ed2ac2a2d
William Faircloth <willuhmjs@gmail.com>
2023-01-30 04:10:38 +0000

    send image before message!

--- src/handlers/welcomeMessageHandler.js
+++ src/handlers/welcomeMessageHandler.js
@@ -6,6 +6,9 @@ export function registerWelcomeHandler(client) {
         if(!welcomeChannelSettings) return;
         const welcomeChannel = member.guild.channels.cache.get(welcomeChannelSettings.channelId);
         if (!welcomeChannel) return;
-        welcomeChannel.send({ files: ['src/assets/welcome-sticker.png'], content: 'Welcome to the server!' });
+        const joinCoursesHere = member.guild.channels.cache.find(channel => channel.name === "join-courses-here");
+        if (!joinCoursesHere) return;
+        await welcomeChannel.send({ files: ['src/assets/welcome-sticker.png'] });
+        await welcomeChannel.send(`Welcome to the server, <@${member.id}>! Please go to <#${joinCoursesHere.id}> to join course chats.`);
     });   
 }
