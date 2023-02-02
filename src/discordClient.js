400dec132d990668e162fdee9e97b2dfde66bf1e
Joel DeSante <joeldesante@gmail.com>
2023-02-01 22:14:46 -0500

    Add GUILD_MEMBERS intent to allow the bot to observe user joins

--- src/discordClient.js
+++ src/discordClient.js
@@ -6,7 +6,8 @@ const client = new Client({
     intents: [ 
         Intents.FLAGS.GUILDS, 
         Intents.FLAGS.GUILD_MESSAGES,
-        Intents.FLAGS.GUILD_MESSAGE_REACTIONS
+        Intents.FLAGS.GUILD_MESSAGE_REACTIONS,
+        Intents.FLAGS.GUILD_MEMBERS
     ],
     partials: ['MESSAGE', 'CHANNEL', 'REACTION'],
 });
