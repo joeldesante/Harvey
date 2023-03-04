e05559200b7d0ef939af4eb02e69f02c7e7cb7b9
William Faircloth <willuhmjs@gmail.com>
2023-03-04 01:07:17 +0000

    feat: finalize command

--- src/commands/updateCourses.js
+++ src/commands/updateCourses.js
@@ -5,17 +5,20 @@ import { messageEmbed } from '../lib/messageEmbed.js';
 
 export default {
     body: new SlashCommandBuilder()
-        .setName('update-course')
+        .setName('update-courses')
         .setDescription('Updates the colors of the course roles in the server.')
         .setDefaultMemberPermissions(0)
         .setDMPermission(false),
     onTriggered: async function(interaction) {
-        updateCourseColors(interaction.guild).then(() => {
-            interaction.reply({ embeds: [messageEmbed(`Updated course colors`, "GREEN")]});
+        console.log("COMMAND RUNS");
+        await interaction.deferReply();
+        try {
+            await updateCourseColors(interaction.guild);
+            interaction.editReply({ embeds: [messageEmbed(`Updated course colors.`, "GREEN")]});
             logger.info(`Updated course colors`);
-        }).catch((err) => {
-            interaction.reply({ embeds: [messageEmbed(`Error updating course colors: ${err}`, "RED")]});
-            logger.error(`Error updating course colors: ${err}`);
+        } catch (err) {
+            interaction.editReply({ embeds: [messageEmbed(`Error updating course colors.`, "RED")]});
+            logger.error(err);
         }
-    }   
+    },
 };
